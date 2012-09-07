e34d4a2073a6b9ad954ce4084f27690969b8cc19
mausch <mauricioscheffer@gmail.com>
2012-09-07 20:06:44 -0300

    Assembly info

---
+++ VariantInterfaces/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace VariantInterfaces
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("0.0.1.0")>]
+[<assembly: AssemblyFileVersion("0.0.1.0")>]
+[<assembly: AssemblyTitle("VariantInterfaces")>]
+[<assembly: AssemblyProduct("VariantInterfaces")>]
+[<assembly: AssemblyDescription("Command-line utility to add variance to interfaces in a .NET assembly.")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2012")>]
+do()
